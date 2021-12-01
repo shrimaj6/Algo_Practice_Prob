2b3ae196e3bb2e8966b1d72213642a997ff694f8
shrimaj mehta <shrimaj.mehta60@gmail.com>
2021-12-01 14:01:57 +0530

    added bubblesort program.

---
+++ BubbleSort1/BubbleSort1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BubbleSort1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int[] arr = { 2, 4, 87, 34 };
+                int temp;
+                for (int j = 0; j <= arr.Length - 2; j++)
+                {
+                    for (int i = 0; i <= arr.Length - 2; i++)
+                    {
+                        if (arr[i] > arr[i + 1])
+                        {
+                            temp = arr[i + 1];
+                            arr[i + 1] = arr[i];
+                            arr[i] = temp;
+                        }
+                    }
+                }
+                Console.WriteLine("Sorted");
+                foreach (int p in arr)
+                {
+                    Console.WriteLine(p + " ");
+                }
+            }
+        }
+    }
+}
