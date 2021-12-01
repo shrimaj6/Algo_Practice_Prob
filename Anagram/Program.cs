4fd787f9863917d467aff9a1b97028fac0e9bafd
shrimaj mehta <shrimaj.mehta60@gmail.com>
2021-12-01 13:23:28 +0530

    Added Anagram Program.

---
+++ Anagram/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Anagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "heater";
+
+            string str2 = "reheat";
+
+            char[] ch1 = str1.ToLower().ToCharArray();
+
+            char[] ch2 = str2.ToLower().ToCharArray();
+
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+
+            String val1 = new string(ch1);
+
+            String val2 = new string(ch2);
+
+            if(val1 == val2)
+            {
+                Console.WriteLine("Anagram-------------{0}----{1}", val1, val2);
+            }
+            else
+            {
+                Console.WriteLine("Not an anagram");
+            }
+        }
+    }
+}
