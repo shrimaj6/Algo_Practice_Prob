66f799d09f26bd56b9ef6384d7ee0ee6c8ed5c6d
shrimaj mehta <shrimaj.mehta60@gmail.com>
2021-12-01 14:38:57 +0530

    Added Generic BubbleSort Program.

---
+++ Generic_BubbleSort/Generic_BubbleSort/BubbleSort.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic_BubbleSort
+{
+
+    class BubbleSort<T> where T : IComparable
+    {
+        private readonly T[] list;
+
+        public BubbleSort(T[] list)
+        {
+            this.list = list;
+        }
+
+        internal void test(T[] list)
+        {
+            bool KeepIterating = true;
+            while (KeepIterating)
+            {
+                KeepIterating = false;
+                for (int i = 0; i < list.Length - 1; i++)
+                {
+                    for (int j = 0; j < list.Length; j++)
+                    {
+                        T x = list[i];
+                        T y = list[i + 1];
+                        if (x.CompareTo(y) > 0)
+                        {
+                            list[i] = y;
+                            list[i + 1] = x;
+                            KeepIterating = true;
+                        }
+                    }
+                }
+            }
+            foreach (T p in list)
+            {
+                Console.Write(p + "-->");
+
+            }
+            Console.Write("\n    ");
+        }
+
+    }
+}
---
+++ Generic_BubbleSort/Generic_BubbleSort/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Generic_BubbleSort
+{
+    class Program
+    { 
+        
+            private static object temp;
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Generic Bubble Sort Program ");
+
+            int[] intlist = { 3, 85, 34, 21, 16 };
+
+            Console.WriteLine(" \n Unsorted List is : 3 85 34 21 16 \n    a  d  z  e  s ");
+
+            Console.Write(" \n Sorted List is : ");
+
+            new BubbleSort<int>(intlist).test(intlist);
+
+            char[] charlist = { 'a', 'd', 'z', 'e', 's' };
+            new BubbleSort<char>(charlist).test(charlist);
+
+            Console.WriteLine("\n \n");
+        }
+    }
+    
+}
